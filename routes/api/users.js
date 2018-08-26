1e33fd1245dfee9d8022b395864a6d504dfac6ed
WingChhun <capt.wing.chhun@gmail.com>
2018-08-25 19:25:32 -0700

    Make user model and make starter CRUD for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
+//* Get User Schema model
+const User = require("../../models/User");
+
+//TODO: User CRUD - START
+
+//TODO: READ ALL
+router.get("/", (req, res) => {});
+
+//TODO: CREATE
+router.post("/", (req, res) => {});
+
+//TODO: UPDATE
+router.post("/:id", (req, res) => {});
+
+//TODO: DELETE
+router.delete("/:id", (req, res) => {});
+
 //TODO GET api/users/test @desc Tests users route @access Public
 router.get("/test", (req, res) => {
 
