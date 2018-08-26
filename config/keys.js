81885a4ae81c65d45b2f0f0fa9c959ee400bf1ed
WingChhun <capt.wing.chhun@gmail.com>
2018-08-25 19:05:16 -0700

    Move keys to config/keys.js directory, add MonmgoDB in server.js

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+const MONGO_URL = "mongodb://Admin:password2@ds127034.mlab.com:27034/devconnector";
+
+const PORT = process.env.PORT || 5000;
+
+module.exports = {
+
+    MONGO_URL,
+    PORT
+}
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
+//DB CONFIG
+const db = require("./config/keys.js").MONGO_URL;
+const PORT = require("./config/keys.js").PORT;
+
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB has connected!"))
+    .catch(err => console.log("MongoDB failed ", err));
+
 //TODO: SETTINGS
 
 //TODO: ROUTES
-
 app.get("/", (req, res) => {
 
     res.send("Hello world");
