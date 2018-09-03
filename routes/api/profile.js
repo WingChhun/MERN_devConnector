0717c213df11995c7e4d7b8442d45f657d1bd473
WingChhun <capt.wing.chhun@gmail.com>
2018-09-03 16:15:30 -0700

    Create Profile Model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const Profile = require("../../models/Profile");
 
-
-//TODO GET api/profile/test
-//@desc Tests profile route
-//@access Public
+//TODO GET api/profile/test @desc Tests profile route @access Public
 router.get("/test", (req, res) => {
 
     const testJSON = {
